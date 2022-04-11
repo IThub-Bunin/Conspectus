bfcee27fa273528f07ed53c671c30e0b4ea0f91b
astrology-noob <0french0fries0@gmail.com>
2022-04-11 17:53:36 +0300

    basic editor page layout

--- editorPage/editorPage.js
+++ editorPage/editorPage.js
@@ -1,3 +1,6 @@
 document.querySelector(".app__content").innerHTML = `
-    <div>Hello world</div>
+    <div>
+        <div class='conspects_menu'>Hello world</div>
+        <div class='editor_block'>Hello world</div>
+    </div>
 `;
