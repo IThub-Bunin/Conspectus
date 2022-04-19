eaff47d86475da056ef5405e0cd6b137e54da0c2
astrology-noob <0french0fries0@gmail.com>
2022-04-19 17:56:57 +0300

    Button "Открыть в редакторе" and scrollbar
    + one icon is deleted

--- bulgakov_pages/addButton.js
+++ bulgakov_pages/addButton.js
@@ -1,6 +1,21 @@
-let theme_title = document.querySelector('.lesson-content-header.lesson-content__header > .lesson-content-header__title');
+// let theme_title = document.querySelector('.lesson-content-header.lesson-content__header > .lesson-content-header__title');
 
-theme_title.addEventListener('DOMSubtreeModified', () => {
-    let task_name = "Задание. " + theme_title.innerText;
-    document.querySelector('.lesson-content__tasks').innerHTML = `<div class="title_status_wrapper"><div class="task_title">${task_name}</div><div class="task_status">Task status</div></div><button class="button_task_conspectus">Открыть в редакторе</button>`;
-});
+// theme_title.addEventListener('DOMSubtreeModified', () => {
+//     let task_name = "Задание. " + theme_title.innerText;
+//     document.querySelector('.lesson-content__tasks').innerHTML = `<div class="title_status_wrapper"><div class="task_title">${task_name}</div><div class="task_status">Task status</div></div><button class="button_task_conspectus">Открыть в редакторе</button>`;
+// });
+
+let node = document.querySelector('.lesson-content__header');
+let editor_button = document.createElement('button');
+editor_button.classList.add('editor_button');
+node.parentNode.insertBefore(editor_button, node);
+
+let editor_a = document.createElement('a');
+editor_a.innerHTML = 'Открыть в редакторе';
+editor_a.setAttribute('href', 'https://ithub.bulgakov.app/conspectus');
+
+editor_button.appendChild(editor_a);
+
+// editor_button.addEventListener('click', () => {
+//     chrome.tabs.create({active: true, url: 'https://ithub.bulgakov.app/conspectus'});
+// });
--- editorPage/generateConspList.js
+++ editorPage/generateConspList.js
@@ -5,6 +5,26 @@ let data_json = `
             "children": [
                 {"label": "file11"},
                 {"label": "file12"},
+                {"label": "file11"},
+                {"label": "file12"},
+                {"label": "file11"},
+                {"label": "file12"},
+                {"label": "file11"},
+                {"label": "file12"},
+                {"label": "file11"},
+                {"label": "file12"},
+                {"label": "file11"},
+                {"label": "file12"},
+                {"label": "file11"},
+                {"label": "file12"},
+                {"label": "file11"},
+                {"label": "file12"},
+                {"label": "file11"},
+                {"label": "file12"},
+                {"label": "file11"},
+                {"label": "file12"},
+                {"label": "file11"},
+                {"label": "file12"},
                 {"label": "file13"}
             ]},
 
@@ -34,7 +54,7 @@ let data = JSON.parse(data_json);
 
 let list_tmp = node => `<div class="item">${node.children ? `<div class="item_label"><span>${node.label}</span><svg class="arrow_icon" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 width="284.929px" height="284.929px" viewBox="0 0 100 100" style="enable-background:new 0 0 284.929 284.929;"
-xml:space="preserve"><g><path d="M7 25 L50 65 L93 25" stroke="black" stroke-width="12" fill="transparent" stroke-linecap="round" /></g></svg></div><div class="children">${node.children.map(list_tmp).join('')}</div>`: `<div class="item_label">${node.label}</div>`}</div>`;
+xml:space="preserve"><g><path d="M7 25 L50 65 L93 25" stroke="black" stroke-width="12" fill="transparent" stroke-linecap="round" /></g></svg></div><div class="children">${node.children.map(list_tmp).join('   ')}</div>`: `<div class="item_label">${node.label}</div>`}</div>`;
 
 let files_list = document.querySelector('.files_menu');
 
