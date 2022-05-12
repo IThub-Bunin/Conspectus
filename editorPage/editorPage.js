51ff75d8b20e165362fe7c9fa3f6434e475afd44
astrology-noob <0french0fries0@gmail.com>
2022-05-12 03:58:26 +0300

    yo it works

--- editorPage/editorPage.js
+++ editorPage/editorPage.js
@@ -6,51 +6,109 @@ document.querySelector('.app__content').innerHTML = `
                 <div class='files_menu'>
                 </div>
             </div>
-            <div class='files_bar'></div>
+            <div class='files_bar'><button class="new_file"><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            viewBox="0 0 463.307 463.307" style="enable-background:new 0 0 463.307 463.307;" xml:space="preserve">
+       <g>
+           <path d="M269.534,399.201c-9.616,0-17.483,7.867-17.483,17.483s7.867,17.483,17.483,17.483h160.263
+               c9.616,0,17.483-7.867,17.483-17.483V138.409c0-4.954-2.04-9.616-5.536-12.821L310.62,4.662C307.414,1.748,303.043,0,298.673,0
+               h-195.23c-9.616,0-17.483,7.867-17.483,17.483v189.402c0,9.616,7.867,17.483,17.483,17.483s17.483-7.867,17.483-17.483V34.967
+               h139.866v151.522c0,9.616,7.867,17.483,17.483,17.483H409.4c0.874,0,2.04,0,2.914-0.291v195.521H269.534z M409.4,169.005H295.759
+               V38.463l116.555,107.522v23.311C411.44,169.005,410.274,169.005,409.4,169.005z M16.026,372.977
+               c0-9.616,7.867-17.483,17.483-17.483h52.45V300.13c0-9.616,7.867-17.483,17.483-17.483s17.483,7.867,17.483,17.483v55.364h58.278
+               c9.616,0,17.483,7.867,17.483,17.483s-7.867,17.483-17.483,17.483h-58.278v55.364c0,9.616-7.867,17.483-17.483,17.483
+               s-17.483-7.867-17.483-17.483V390.46H33.51C23.894,390.46,16.026,382.592,16.026,372.977z"/>
+       </g></svg></button></div>
         </div>
         <div class='editor_block'>
             <div class='labels'>
             </div>
             <div class='editor_area'>
-                <div id="editor-md"></div>
+                <textarea id="editor-md"></textarea>
+                <div class="buttons_bar">
+                    <button class="save_button">Сохранить</button>
+                </div>
             </div>
         </div>
     </div>
 `;
 
-// Вставить зависимости в сайт
-// let chrome_editor = chrome.runtime.getURL("editor-md");
-// console.log(chrome.runtime.getURL("editor-md/lib/codemirror"));
-
-// document.body.innerHTML += `
-//     <script src="${chrome_editor}/jquery.min.js"></script>
-//     <script src="${chrome_editor}/editormd.min.js"></script>
-// `
-
-// функция для изменения "открытой вкладки"
-function makePrimary(event) {
-    if (primary_label) {
-        primary_label.classList.remove('primary_label');
-    }
-    event.target.classList.add('primary_label');
-    primary_label = event.target;
-}
-
 // Список названий ярлыков, элемент-обёртка для ярлыков, выбранный ярлык
 let labels = [];
 let labels_el = document.querySelector('.labels');
-let primary_label = null;
 
-// // Генерация ярлыков
-// for (let i = 0; i < labels.length; i++) {
-//     labels_el.innerHTML += `<div class='label'>${labels[i]}</div>`;
-// }
+let active_conspects = [];
+let cur_conspect = {"name": null, "data": null};
 
-// Добавляем событие по клику мышкой
-Array.from(labels_el.children).forEach(element => {
-    element.addEventListener('click', makePrimary)
-});
+function removeLabel(event) {
+    // labels.pop(labels.indexOf(event.target.parentNode.firstChild.innerHTML));
+    // labels_el.removeChild(event.target.parentNode.parentNode);
+    // active_conspects.pop(active_conspects.indexOf(cur_conspect));
+    
+    // if (labels.length >= 1) {
+    //     document.querySelector(".labels>:last-child").classList.add('primary_label');
+    //     active_conspects.forEach(consp => {if (consp.name == labels_el.lastChild.firstChild.innerHTML) {cur_conspect = consp}});
+    // }
+    // else { 
+    //     cur_conspect = {"name": null, "data": null}
+    // }
+}
 
-// // Делаем первый ярлык активным
-// primary_label = labels_el.lastChild;
-// labels_el.lastChild.classList.add('primary_label');
+function makePrimary(label) {
+    if (!Array.from(label.classList).includes("primary_label")) {
+        primary_label = document.querySelector(".primary_label");
+        if (primary_label) {
+            active_conspects[labels.indexOf(primary_label.firstChild.innerHTML)]["data"] = document.querySelector("textarea").value;
+            primary_label.classList.remove("primary_label");
+        }
+        label.classList.add("primary_label");
+        consp_name = label.firstChild.innerHTML;
+        cur_conspect["name"] = consp_name;
+        cur_conspect["data"] = active_conspects[labels.indexOf(consp_name)]["data"];
+        document.querySelector("textarea").value = cur_conspect["data"];
+    }
+    // console.log(cur_conspect);
+    // if (document.querySelector(".primary_label")) {
+    //     document.querySelector(".primary_label").classList.remove('primary_label');
+    // }
+    // event.element.classList.add('primary_label');
+    // active_conspects.forEach(consp => {if (consp.name == event.element.innerHTML) {cur_conspect = consp}});
+    // console.log(cur_conspect);
+    // document.querySelector("textarea").innerHTML = cur_conspect.data;
+}
+
+function openConspect(event) {
+    // if(!labels.includes(event.target.innerHTML)) {
+    //     labels_el.innerHTML += `<div class='label'><p>${event.target.innerHTML}</p><button><svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><defs><style>.cls-1{fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:2px;}</style></defs><title/><g id="cross"><line class="cls-1" x1="7" x2="25" y1="7" y2="25"/><line class="cls-1" x1="7" x2="25" y1="25" y2="7"/></g></svg></button></div>`;
+    //     document.querySelector(".labels>:last-child>:last-child").addEventListener('click', removeLabel);
+    //     document.querySelector(".labels>:last-child").addEventListener('click', makePrimary);
+    //     if (document.querySelector(".primary_label")) {
+    //         document.querySelector(".primary_label").classList.remove('primary_label');
+    //     }
+    //     document.querySelector(".labels>:last-child").classList.add('primary_label');
+    //     labels.push(event.target.innerHTML);
+    //     // data = getConspectData(username, event.target.innerHTML);
+    //     // data = getConspectData(username, event.target.innerHTML);
+
+    //     document.querySelector("textarea").innerHTML = event.target.innerHTML;
+    
+    //     cur_conspect = {"name": event.target.innerHTML, "data": event.target.innerHTML};
+    //     active_conspects.push(cur_conspect);
+    // }
+    consp_name = event.target.innerHTML;
+    if(!labels.includes(consp_name)) {
+        labels_el.innerHTML += `<div class='label'><p>${consp_name}</p><button><svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><defs><style>.cls-1{fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:2px;}</style></defs><title/><g id="cross"><line class="cls-1" x1="7" x2="25" y1="7" y2="25"/><line class="cls-1" x1="7" x2="25" y1="25" y2="7"/></g></svg></button></div>`;
+        labels.push(consp_name);
+        active_conspects[labels.length - 1] = {"name": null, "data": null};
+        active_conspects[labels.length - 1]["name"] = consp_name;
+        getConspectData(username, consp_name.slice(1, consp_name.length - 3)).then(text => {
+            active_conspects[labels.length - 1]["data"] = text;
+            Array.from(document.querySelectorAll('.label')).forEach(element => {
+                element.onclick = (event) => {makePrimary(event.target)}
+            });
+            makePrimary(document.querySelector('.labels>:last-child'));
+        });
+    }
+}
+
+document.querySelector(".new_file").addEventListener("click", () => {conspect = prompt("Введите название конспекта", "RandomName"); createConspect(username, conspect)});
+document.querySelector(".save_button").addEventListener("click", () => {setConspectData(username)});
--- editorPage/generateConspList.js
+++ editorPage/generateConspList.js
@@ -1,21 +1,17 @@
-let res = ["file1", "file2", "file3"];
-
 let data_json = `
     {   "label": "Список конспектов",
-        "children": [`;
-
-for (let consp of res) {
-    data_json += `{"label": "${consp}"},`
-}''
-
-data_json = data_json.slice(0, data_json.length-1);
-
-data_json += `
-        ]
-    }
-`;
+        "children": []
+    }`;
 
 let data = JSON.parse(data_json);
+
+let username = document.querySelector(".top-menu__user-profile__link__name").innerHTML.trim();
+
+if (!userExist(username)) {
+    createUser(username);
+}
+
+// result = ["gile", "kile", "mile"]
 
 let list_tmp = node => `<div class="item">${node.children ? `<div class="item_label"><span>${node.label}</span><svg class="arrow_icon" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 width="284.929px" height="284.929px" viewBox="0 0 100 100" style="enable-background:new 0 0 284.929 284.929;"
@@ -23,50 +19,34 @@ xml:space="preserve"><g><path d="M7 25 L50 65 L93 25" stroke="black" stroke-widt
 
 let files_list = document.querySelector('.files_menu');
 
-files_list.innerHTML = list_tmp(data);
 
-function changeVisibility() {
-    if (Array.from(this.nextSibling.classList).includes('children__active')) {
-        this.classList.remove('item_label__active');
-        this.nextSibling.classList.remove('children__active');
-        this.lastChild.firstChild.firstChild.setAttribute("stroke", "black");
-    }
-    else {
-        this.classList.add('item_label__active');
-        this.nextSibling.classList.add('children__active');
-        this.lastChild.firstChild.firstChild.setAttribute("stroke", "#0080ff");
-    }
-}
-
-function removeLabel(event) {
-    labels.pop(labels.indexOf(event.target.parentNode.firstChild.innerHTML));
-    console.log(event.target.parentNode);
-    labels_el.removeChild(event.target.parentNode.parentNode)
-}
-
-function addLabel(event) {
-    if(!labels.includes(event.target.innerHTML)) {
-        labels_el.innerHTML += `<div class='label'><p>${event.target.innerHTML}</p><button><svg class="arrow_icon" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-        width="284.929px" height="284.929px" viewBox="0 0 100 100" style="enable-background:new 0 0 284.929 284.929;"
-        xml:space="preserve"><g><path d="M7 25 L50 65 L93 25" stroke="black" stroke-width="12" fill="transparent" stroke-linecap="round" /></g></svg></button></div>`;
-        labels_el.lastChild.lastChild.addEventListener('click', removeLabel);
-        if (labels.length > 0) {
-            primary_label.classList.remove('primary_label');
+let res = getConspectsOf(username);
+res.then(consps => {for (let file of consps) {data.children.push({"label": `${file}`})}})
+    .then(resolved => {
+        files_list.innerHTML = list_tmp(data)
+        
+        function changeVisibility() {
+            if (Array.from(this.nextSibling.classList).includes('children__active')) {
+                this.classList.remove('item_label__active');
+                this.nextSibling.classList.remove('children__active');
+                this.lastChild.firstChild.firstChild.setAttribute("stroke", "black");
+            }
+            else {
+                this.classList.add('item_label__active');
+                this.nextSibling.classList.add('children__active');
+                this.lastChild.firstChild.firstChild.setAttribute("stroke", "#0080ff");
+            }
         }
-        primary_label = labels_el.lastChild;
-        primary_label.classList.add('primary_label');
-        labels.push(event.target.innerHTML);
-    }
-}
-
-Array.from(document.querySelectorAll('.item_label')).forEach(element => {
-    if (element.nextSibling) {
-        element.addEventListener('click', changeVisibility);
-    }
-    else {
-        element.addEventListener('click', addLabel);
-    }
-});
+        
+        Array.from(document.querySelectorAll('.item_label')).forEach(element => {
+            if (element.nextSibling) {
+                element.addEventListener('click', changeVisibility);
+            }
+            else {
+                element.addEventListener('click', openConspect);
+            }
+        });
+    });
 
 // let link = browser.runtime.getURL("angle-arrow-down_icon-icons.com_73683.svg");
 // Array.from(document.querySelectorAll('.arrow_icon')).forEach(element => {
